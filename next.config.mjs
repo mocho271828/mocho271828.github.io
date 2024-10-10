2441535258fa72ac060a2e5119fe225a08dfb49b
mocho2718281 <165557503+mocho271828@users.noreply.github.com>
2024-10-10 13:59:29 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-+ output: "export",
-}
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // basePath を設定（リポジトリ名がプロジェクト名と同じ場合）
+  basePath: '/mocho271828.github.io',
+};
 
-export default nextConfig
+export default nextConfig;
