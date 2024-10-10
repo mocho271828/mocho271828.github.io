0aa29c9d0f2e422625005629f64aaffa0683a6dd
mocho2718281 <165557503+mocho271828@users.noreply.github.com>
2024-10-10 15:55:36 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useRouter } from "next/router";
+import React, { useEffect } from 'react'
 
+import { existsGaId, pageview } from '../src/lib/gtag'
 import GoogleAnalytics from '../src/components/GoogleAnalytics'
 
 const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!existsGaId) {
+      return
+    }
+
+    const handleRouteChange = (path) => {
+      pageview(path)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <GoogleAnalytics />
