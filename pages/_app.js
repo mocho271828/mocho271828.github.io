1a593beb4f43caf4ff5a2bc195940e2b9e16d7df
mocho2718281 <165557503+mocho271828@users.noreply.github.com>
2024-10-10 15:43:02 +0900

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import React from 'react'
+
+import { GA_ID, existsGaId } from '../src/lib/gtag'
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        {/* Google Analytics */}
+        {existsGaId && (
+          <>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}', {
+                    page_path: window.location.pathname,
+                  });`,
+              }}
+            />
+          </>
+        )}
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
