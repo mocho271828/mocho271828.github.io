5be1b01e772f7949a539e7d5386dd9669e4f4f31
mocho2718281 <165557503+mocho271828@users.noreply.github.com>
2024-10-10 11:22:28 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,8 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  reactStrictMode: true,
-  
-  // GitHub Pages用の設定
-  basePath: process.env.GITHUB_ACTIONS ? `/${process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''}` : '',
-  
-  // 画像最適化の設定
-  images: {
-    loader: 'custom',
-    loaderFile: './imageLoader.js',
-  },
++ output: "export",
+}
 
-  // 静的エクスポートの設定
-  output: 'export',
-};
-
-export default nextConfig;
+export default nextConfig
