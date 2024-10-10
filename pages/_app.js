7a521ccdc9409f67718e2f12e574b3d9bc29ffec
mocho2718281 <165557503+mocho271828@users.noreply.github.com>
2024-10-10 15:51:27 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,11 @@
-import Head from "next/head";
 import React from 'react'
 
-import { GA_ID, existsGaId } from '../src/lib/gtag'
+import GoogleAnalytics from '../src/components/GoogleAnalytics'
 
 const App = ({ Component, pageProps }) => {
   return (
     <>
-      <Head>
-        {/* Google Analytics */}
-        {existsGaId && (
-          <>
-            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${GA_ID}', {
-                    page_path: window.location.pathname,
-                  });`,
-              }}
-            />
-          </>
-        )}
-      </Head>
+      <GoogleAnalytics />
 
       <Component {...pageProps} />
     </>
