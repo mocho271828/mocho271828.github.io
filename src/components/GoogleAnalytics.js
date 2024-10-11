89f8a70354c178ee2d6e0b6d11b334e07692085f
mocho2718281 <165557503+mocho271828@users.noreply.github.com>
2024-10-11 13:17:14 +0900

    Update GoogleAnalytics.js

--- src/components/GoogleAnalytics.js
+++ src/components/GoogleAnalytics.js
@@ -1,22 +1,48 @@
+import { useRouter } from 'next/router'
 import Script from 'next/script'
-import { existsGaId, GA_ID } from '../lib/gtag'
+import { useEffect } from 'react'
+import { GA_ID, existsGaId, pageview } from '../lib/gtag'
 
-const GoogleAnalytics = () => (
+export const usePageView = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!existsGaId) {
+      return
+    }
+
+    const handleRouteChange = (path: string) => {
+      pageview(path)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+}
+
+export const GoogleAnalytics = () => (
   <>
     {existsGaId && (
       <>
-        <Script defer src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} strategy="afterInteractive" />
-        <Script id="ga" defer strategy="afterInteractive">
-          {`
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script
+          id="ga-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());    
               gtag('config', '${GA_ID}');
-          `}
-        </Script>
+            `,
+          }}
+        />
       </>
     )}
   </>
 )
-
-export default GoogleAnalytics
