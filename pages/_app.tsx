26b7992dbb67224499f98d03fc6adb564d67d9c3
mocho2718281 <165557503+mocho271828@users.noreply.github.com>
2024-10-11 13:16:14 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app'
+import { GoogleAnalytics, usePageView } from '../src/components/GoogleAnalytics'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  usePageView() // Google Analyticsのページビューを追跡
+
+  return (
+    <>
+      <GoogleAnalytics />
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default MyApp
