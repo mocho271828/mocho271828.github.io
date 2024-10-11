6f756031fbedf91143d1d0359e4cc29db3abe765
mocho2718281 <165557503+mocho271828@users.noreply.github.com>
2024-10-11 16:59:48 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,13 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Inter } from 'next/font/google'
 import { GoogleAnalytics, usePageView } from '../src/components/GoogleAnalytics'
-const inter = Inter({ subsets: ['latin'] })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  usePageView() // Google Analyticsのページビューを追跡
-
+  usePageView()
   return (
     <>
       <GoogleAnalytics />
       <Component {...pageProps} />
     </>
-    <main className={inter.className}>
-      <Component {...pageProps} />
-    </main>
   )
 }
 
