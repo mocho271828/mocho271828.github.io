99b62b1f23eb783922829a654f85ce334ce919a9
mocho2718281 <165557503+mocho271828@users.noreply.github.com>
2024-10-14 01:17:18 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-}
-  reactStrictMode: true,
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/mocho271828.github.io',
-};
+}
 
 export default nextConfig
-export default nextConfig;
